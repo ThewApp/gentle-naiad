c210028610af61c69ee0e27ab60d7e3e75467bdd
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2018-12-26 14:04:12 +0700

    Fix train.py

--- train/train.py
+++ train/train.py
@@ -1,13 +1,16 @@
+import os
 from rasa_core.agent import Agent
 
+
+dirname = os.path.dirname(__file__)
 
 def train_dialogue(domain_file="domain.yml",
                    model_path="models/dialogue",
                    training_data_file="stories.md"):
-    agent = Agent(domain_file)
+    agent = Agent(os.path.join(dirname, domain_file))
 
-    training_data = agent.load_data(training_data_file)
-    agent.train(training_data)
+    training_data = agent.load_data(os.path.join(dirname, training_data_file))
+    agent.train(os.path.join(dirname, training_data))
 
     agent.persist(model_path)
     return agent
