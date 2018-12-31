bcb18bd36bc164188cbde6d2ef57c6c08217ea86
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2018-12-31 17:52:27 +0700

    Fix logging

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ import logging
 
 ENV = os.getenv('ENV', 'PRODUCTION')
 
-if ENV == "DEVELOPMENT":
-    import ptvsd
+if ENV == "LOCAL_DEVELOPMENT":
+    logging_level = logging.DEBUG
 
+    import ptvsd
     # 5678 is the default attach port in the VS Code debug configurations
     print("Waiting for debugger attach")
     ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
     ptvsd.wait_for_attach()
+elif ENV == "DEVELOPMENT":
+    logging_level = logging.INFO
 
-logging_level = logging.INFO if ENV == "DEVELOPMENT" else logging.WARNING
 logging.basicConfig(level=logging_level)
 
 logger = logging.getLogger(__name__)
