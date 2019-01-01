fa580af847ef54f5e52903036bdf991945a57fee
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2019-01-01 18:54:41 +0700

    Add redis tracker store

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from rasa_core.interpreter import RasaNLUInterpreter
 from rasa.lineagent import LineAgent
 from rasa.lineconnector import LineInput
+from rasa_core.tracker_store import RedisTrackerStore
 
 import os
 import logging
@@ -22,13 +23,18 @@ logging.basicConfig(level=logging_level)
 
 logger = logging.getLogger(__name__)
 
-# load your trained agent
-agent = LineAgent.load(
-    "models/dialogue", interpreter=RasaNLUInterpreter("models/current/nlu"))
-
 line_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 line_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-port = int(os.getenv('PORT', None))
+port = int(os.getenv('PORT', 8080))
+redis_url = os.getenv('REDIS_URL', None)
+
+tracker_store = RedisTrackerStore(None, redis_url) if redis_url else None
+
+agent = LineAgent.load(
+    "models/dialogue",
+    interpreter=RasaNLUInterpreter("models/current/nlu"),
+    tracker_store=tracker_store
+)
 
 input_channel = LineInput(
     line_secret=line_secret,
