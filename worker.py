2db11fa3712d4291a9b10810d2db8974cb7a2e06
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2019-01-02 23:25:09 +0700

    Test reminder #13

--- worker.py
+++ worker.py
@@ -1,12 +1,17 @@
 from rasa.store import scheduler_store
+from rq import Worker, Queue, Connection
 from rq_scheduler.scheduler import Scheduler
-from rq_scheduler.utils import setup_loghandlers
+import threading
 
-setup_loghandlers('DEBUG')
+listen = ['high', 'default', 'low']
 
 if __name__ == '__main__':
     scheduler = Scheduler(
         connection=scheduler_store,
         interval=5
     )
-    scheduler.run()
+    running_scheduler = threading.Thread(target=scheduler.run)
+    running_scheduler.start()
+    with Connection(scheduler_store):
+        worker = Worker(map(Queue, listen))
+        worker.work()
