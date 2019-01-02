a0bbac18080d4ef3658f968410fde3bfd224c895
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2019-01-02 22:32:11 +0700

    Test reminder #11

--- worker.py
+++ worker.py
@@ -2,5 +2,8 @@ from rq_scheduler.scheduler import Scheduler
 from rasa.store import scheduler_store
 
 if __name__ == '__main__':
-    scheduler = Scheduler(connection=scheduler_store)
+    scheduler = Scheduler(
+        connection=scheduler_store,
+        interval=5
+    )
     scheduler.run()
