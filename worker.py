4f300e89ba268b8987b0a39c8a4ffeb6463c88f9
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2019-01-02 22:43:19 +0700

    Test reminder #12

--- worker.py
+++ worker.py
@@ -1,5 +1,8 @@
-from rq_scheduler.scheduler import Scheduler
 from rasa.store import scheduler_store
+from rq_scheduler.scheduler import Scheduler
+from rq_scheduler.utils import setup_loghandlers
+
+setup_loghandlers('DEBUG')
 
 if __name__ == '__main__':
     scheduler = Scheduler(
