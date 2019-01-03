0045c606831ba0fd97ef7b4131137d94a02d5d83
TheBear44 <26767607+TheBear44@users.noreply.github.com>
2019-01-03 10:20:17 +0700

    Test reminder #19

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python app.py
-worker: python worker.py
+web: python app.py web
+worker: python app.py worker
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from rasa_core.interpreter import RasaNLUInterpreter
 from rasa.lineagent import LineAgent
 from rasa.lineconnector import LineInput
-from rasa.store import tracker_store
+from rasa.store import tracker_store, scheduler_store
+from rq import Worker, Queue, Connection
+from rq_scheduler.scheduler import Scheduler
+from multiprocessing import Process
 
-import os
+import argparse
 import logging
+import os
 
 ENV = os.getenv('ENV', 'PRODUCTION')
 
@@ -29,7 +33,6 @@ logger = logging.getLogger(__name__)
 
 line_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 line_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-port = int(os.getenv('PORT', 8080))
 
 agent = LineAgent.load(
     "models/dialogue",
@@ -37,11 +40,35 @@ agent = LineAgent.load(
     tracker_store=tracker_store
 )
 
-input_channel = LineInput(
-    line_secret=line_secret,
-    line_access_token=line_access_token
-)
+def webapp():
+    port = int(os.getenv('PORT', 8080))
 
-logger.info("Ready")
+    input_channel = LineInput(
+        line_secret=line_secret,
+        line_access_token=line_access_token
+    )
 
-app = agent.handle_channels([input_channel], http_port=port)
+    logger.info("Web is ready.")
+
+    agent.handle_channels([input_channel], http_port=port)
+
+def worker():
+    listen = ['high', 'default', 'low']
+    scheduler = Scheduler(
+        connection=scheduler_store,
+        interval=5
+    )
+    Process(target=scheduler.run).start()
+    with Connection(scheduler_store):
+        worker = Worker(map(Queue, listen))
+        Process(target=worker.work).start()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='MeDiary Application')
+    parser.add_argument('type', default="web", help='Process type')
+
+    args = parser.parse_args()
+    if args.type == "web":
+        webapp()
+    elif args.type == "worker":
+        worker()
